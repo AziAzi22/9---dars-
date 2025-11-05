b25bd9c8d5ffaf195e9d0b2e78b75c0fd55df883
AziAzi22 <aziazi22t@gmail.com>
2025-11-05 19:21:53 +0500

    ppqq

--- dars.js
+++ dars.js
@@ -1,7 +1,6 @@
 // qo'shimcha masalalar
 
-
-// 1 - masala 
+// 1 - masala
 
 // The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
@@ -9,7 +8,7 @@
 // "din"      =>  "((("
 // "recede"   =>  "()()()"
 // "Success"  =>  ")())())"
-// "(( @"     =>  "))((" 
+// "(( @"     =>  "))(("
 // Notes
 // Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
@@ -31,7 +30,7 @@
 // console.log(duplicateEncode("Success"));
 // console.log(duplicateEncode("(( @"));
 
-// 2 - masala 
+// 2 - masala
 
 // In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
 // The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
@@ -73,7 +72,7 @@
 // decodeMorse = function(morseCode){
 //   // you can use the preloaded MORSE_CODE:
 //   // let letter = MORSE_CODE[morse]
-  
+
 //   // Your code here
 //   morseCode = morseCode.trim().split("   ")
 //   let result = []
@@ -89,29 +88,28 @@
 //   }
 //   console.log(decodeMorse('.... . -.--   .--- ..- -.. .'));
 
-  //////////  buni bu yerda tekahirib bo'lmyadi ustoz,  MORSE_CODE berilmagan, saytga kirib ko'rsangiz bo'ladi : https://www.codewars.com/kata/54b724efac3d5402db00065e/train/javascript
-  
+//////////  buni bu yerda tekahirib bo'lmyadi ustoz,  MORSE_CODE berilmagan, saytga kirib ko'rsangiz bo'ladi : https://www.codewars.com/kata/54b724efac3d5402db00065e/train/javascript
 
-  // 3 - masala
+// 3 - masala
 
 //   Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
 
 // Examples
 // Valid arrays
-// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 // comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
 
-// a = [121, 144, 19, 161, 19, 144, 19, 11] 
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
 // Invalid arrays
 // If, for example, we change the first number to something else, comp is not returning true anymore:
 
-// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
 // comp(a,b) returns false because in b 132 is not the square of any number of a.
 
-// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
 // comp(a,b) returns false because in b 36100 is not the square of any number of a.
 
@@ -141,5 +139,62 @@
 // }
 // console.log(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]));
 
-// 4 - masala 
+// 4 - masala
+
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// function solution(string) {
+//   if(string.length <= 1) return string
+//   let result = ""
+//   for(let i = 0; i < string.length; i++){
+//    if(/[A-Z]/.test(string[i])){
+//      result += " " + string[i]
+//    }else{
+//      result += string[i]
+//    }
+//   }
+//   return result
+// }
+// console.log(solution("camelCasing"));
+// console.log(solution("identifier"));
+// console.log(solution(""));
+
+// 5 - masala
+
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway
+
+// function pigIt(str) {
+//   //Code here
+//   let result = "";
+//   let x = null;
+//   for (let i = 0; i < str.length; i++) {
+//     if (i === 0 || str[i - 1] === " ") {
+//       if (/[a-zA-Z]/.test(str[i])) {
+//         x = str[i] + "ay";
+//       } else {
+//         x = str[i];
+//       }
+//     } else if (str[i] === " ") {
+//       result += x + " ";
+//       x = null;
+//     } else {
+//       result += str[i];
+//     }
+//   }
+//   if (x !== null) result += x;
+//   return result;
+// }
+// console.log(pigIt("Pig latin is cool"))
+// console.log(pigIt("Hello world !"));
+
+// 6 - masala
 
